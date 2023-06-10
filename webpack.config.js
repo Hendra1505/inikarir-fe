7329f6f8fd8d0245cd572a18a53619be857a4439
Poralia <cli.exec.com@gmail.com>
2023-06-10 20:58:18 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	mode: 'development',
+	entry: './src/static/js/main.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devServer: {
+		static: path.resolve(__dirname, 'dist'),
+		port: 9090,
+		hot: true,
+		compress: false,
+		historyApiFallback: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ 
+			filename: 'index.html',
+			template: './src/index.html',
+			chunks: ['main'],
+		}),
+		new HtmlWebpackPlugin({ 
+			filename: 'detail.html',
+			template: './src/detail.html',
+			chunks: ['main'],
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(scss)$/,
+				use: [
+					{
+						// Adds CSS to the DOM by injecting a `<style>` tag
+						loader: 'style-loader'
+					},
+					{
+						// Interprets `@import` and `url()` like `import/require()` and will resolve them
+						loader: 'css-loader'
+					},
+					{
+						// Loader for webpack to process CSS with PostCSS
+						loader: 'postcss-loader',
+						options: {
+						postcssOptions: {
+							plugins: () => [
+								autoprefixer
+							]
+						}
+						}
+					},
+					{
+						// Loads a SASS/SCSS file and compiles it to CSS
+						loader: 'sass-loader'
+					}
+				],
+			},
+		],
+  	},
+}
